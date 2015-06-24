d03823c7cda880a3b1459f5aa704f2f67653f8c8
Brian Whicheloe <brian@snapshot.is>
2015-06-24 13:13:34 -0700

    Add highlighting to UI and move some things around

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var doc_width = $(window).width();
 var doc_height = $(window).height();
 var terrain = new Terrain('main');
-var ui = new UI('main');
+var ui = new UI('ui');
 var needs_update = true;
 
 $(document).ready(function(){
@@ -15,25 +15,27 @@ $(document).ready(function(){
 
 	ui.click_listener();
 	ui.pan_listener();
+	ui.highlight_tile();
 
 	terrain.start_path();
-	
+
 	setInterval(function(){
 		if(needs_update){
 			terrain.clear_map();
 			terrain.start_path();
 			var i = 0;
 			while(i < 10){
-				terrain.draw_tile(i*51,0);
-				
+				terrain.draw_tile(i*50,0);
+
 				var i2 = 0;
 				while(i2 < 10){
-					terrain.draw_tile(i*51,i2*51);
+					terrain.draw_tile(i*50,i2*50);
 					i2++;
 				}
 				i++;
 			}
 			terrain.fill();
+			terrain.stroke();
 		}
 		needs_update = false;
 	}, 10);
@@ -44,10 +46,12 @@ function Terrain(terrain_canvas_id){
 
 	this.terrain_canvas = document.getElementById(terrain_canvas_id);
 	this.terrain_ctx = this.terrain_canvas.getContext('2d');
-	
+
 	this.window_width = $(window).width();
 	this.window_height = $(window).height();
 	this.tile_width = 50;
+
+	this.translation = [0,0];
 
 	this.start_path = function(){
 		this.terrain_ctx.beginPath();
@@ -56,6 +60,10 @@ function Terrain(terrain_canvas_id){
 
 	this.fill = function(){
 		this.terrain_ctx.fill();
+	}
+
+	this.stroke = function(){
+		this.terrain_ctx.stroke();
 	}
 
 	this.clear_map = function(){
@@ -97,6 +105,30 @@ function Terrain(terrain_canvas_id){
 		this.terrain_ctx.lineTo(points[0], points[1]);
 	}
 
+	this.translate_map = function(difference_x, difference_y){
+		this.translation[0] += difference_x;
+		this.translation[1] += difference_y;
+		this.terrain_ctx.translate(difference_x, difference_y);
+		// console.log( terrain.translation );
+	}
+
+	this.get_tile_corners = function(canvas_x, canvas_y){
+		var bounding_box = new Array();
+		var coords = this.iso_to_cartesian(canvas_x, canvas_y);
+
+		coords[0] = Math.floor( coords[0] / this.tile_width ) * this.tile_width;
+		coords[1] = Math.floor( coords[1] / this.tile_width ) * this.tile_width;
+
+
+		bounding_box[0] = this.cartesian_to_iso( coords[0], coords[1] );
+		bounding_box[1] = this.cartesian_to_iso( coords[0]+this.tile_width, coords[1] );
+		bounding_box[2] = this.cartesian_to_iso( coords[0]+this.tile_width, coords[1]+this.tile_width );
+		bounding_box[3] = this.cartesian_to_iso( coords[0], coords[1]+this.tile_width );
+
+
+		return bounding_box;
+	}
+
 }
 
 function UI(ui_canvas_id){
@@ -107,7 +139,7 @@ function UI(ui_canvas_id){
 	this.last_page_x;
 	this.last_page_y;
 	this.mouse_is_down = false;
-	
+
 	this.last_x;
 	this.last_y;
 
@@ -119,7 +151,9 @@ function UI(ui_canvas_id){
 				var tmp_difference_x = e.pageX - self.last_x;
 				var tmp_difference_y = e.pageY - self.last_y;
 
-				self.ui_ctx.translate(tmp_difference_x, tmp_difference_y);
+				// self.ui_ctx.translate(tmp_difference_x, tmp_difference_y);
+				terrain.translate_map(tmp_difference_x, tmp_difference_y);
+
 				self.last_x = e.pageX;
 				self.last_y = e.pageY;
 				needs_update = true;
@@ -136,6 +170,28 @@ function UI(ui_canvas_id){
 
 		$('#'+this.ui_id).mouseup(function(e){
 			self.mouse_is_down = false;
+		});
+	}
+
+	this.clear_ui = function(){
+		this.ui_ctx.clearRect(0,0,doc_width,doc_height);
+	}
+
+	this.highlight_tile = function(){
+		self.ui_ctx.fillStyle = 'rgba(0,100,0,0.5)';
+
+		$('#'+this.ui_id).mousemove(function(e){
+			var bounding_box = terrain.get_tile_corners(e.pageX + terrain.translation[0], e.pageY + terrain.translation[1]);
+
+			self.clear_ui();
+			self.ui_ctx.beginPath();
+			self.ui_ctx.moveTo( bounding_box[0][0], bounding_box[0][1] );
+			self.ui_ctx.lineTo( bounding_box[1][0], bounding_box[1][1] );
+			self.ui_ctx.lineTo( bounding_box[2][0], bounding_box[2][1] );
+			self.ui_ctx.lineTo( bounding_box[3][0], bounding_box[3][1] );
+			self.ui_ctx.closePath();
+			// self.ui_ctx.stroke();
+			self.ui_ctx.fill();
 		});
 	}
 }
