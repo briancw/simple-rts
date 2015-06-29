71fcfe7fa6e78a5353981dc3dd80eeebe3bc8b86
Brian Whicheloe <brian@whicheloe.us>
2015-06-28 23:04:35 -0700

    Added some centering and almost perfect automatic width calculation

--- main.js
+++ main.js
@@ -1,10 +1,42 @@
 var doc_width = $(window).width();
 var doc_height = $(window).height();
-var terrain = new Terrain('main');
+var doc_diagonal = Math.round(Math.sqrt( Math.pow(doc_width,2) + Math.pow(doc_height,2) ));
+var resolution = 50;
+var cube_size = Math.ceil(doc_diagonal / resolution);
+cube_size %2 == 0 ? cube_size : cube_size++;
+console.log(cube_size);
+// var cube_size = 20;
+var origin = [5,5];
+
+console.log( cube_size );
+
+var terrain = new Terrain('main', resolution);
 var ui = new UI('ui');
 var needs_update = true;
 
+var server_url = 'http://localhost:8080';
+
 $(document).ready(function(){
+
+	$.ajax({
+		url: server_url+'/heightmap',
+		type : 'POST',
+		data: {'cube_size': cube_size},
+		dataType: 'JSON',
+		success: function(ret){
+			if( typeof(ret) !== 'undefined' ){
+				if( typeof(ret.heightmap) !== 'undefined' && ret.heightmap.length ){
+
+					// var heightmap = ret.heightmap;
+					// draw_heightmap(heightmap);
+
+					terrain.draw_tilemap(ret.heightmap);
+
+				}
+			}
+		}
+	});
+
 
 	$('.canvas').each(function(){
 		$(this).attr('width', doc_width);
@@ -19,39 +51,101 @@ $(document).ready(function(){
 
 	terrain.start_path();
 
-	setInterval(function(){
-		if(needs_update){
-			terrain.clear_map();
-			terrain.start_path();
-			var i = -9;
-			while(i < 10){
-				terrain.draw_tile(i*50,0);
+	// setInterval(function(){
+	// 	if(needs_update){
+	// 		terrain.clear_map();
+	// 		terrain.start_path();
+	// 		var i = -9;
+	// 		while(i < 10){
+	// 			terrain.draw_tile(i*50,0);
 
-				var i2 = -9;
-				while(i2 < 10){
-					terrain.draw_tile(i*50,i2*50);
-					i2++;
-				}
-				i++;
-			}
-			terrain.fill();
-			terrain.stroke();
-		}
-		needs_update = false;
-	}, 10);
+	// 			var i2 = -9;
+	// 			while(i2 < 10){
+	// 				terrain.draw_tile(i*50,i2*50);
+	// 				i2++;
+	// 			}
+	// 			i++;
+	// 		}
+	// 		terrain.fill();
+	// 		terrain.stroke();
+	// 	}
+	// 	needs_update = false;
+	// }, 10);
 
 });
 
-function Terrain(terrain_canvas_id){
+function Terrain(terrain_canvas_id, resolution){
 
 	this.terrain_canvas = document.getElementById(terrain_canvas_id);
 	this.terrain_ctx = this.terrain_canvas.getContext('2d');
 
 	this.window_width = $(window).width();
 	this.window_height = $(window).height();
-	this.tile_width = 50;
+	this.tile_width = resolution;
 
 	this.translation = [0,0];
+
+	this.draw_tilemap = function(tilemap){
+		var cube_size = Math.sqrt(tilemap.length);
+
+		var start_x = -(cube_size/2) * this.tile_width;
+		var start_y = -(cube_size/2) * this.tile_width;
+		var end_x = (cube_size * 0.5) * this.tile_width;
+		var end_y = (cube_size * 0.5) * this.tile_width;
+
+		for(var ix = start_x; ix < end_x; ix += this.tile_width){
+			for(var iy = start_y; iy < end_y; iy += this.tile_width){
+
+				this.draw_tile(ix, iy);
+
+			}
+		}
+
+		this.terrain_ctx.fill();
+
+	}
+
+	// this.draw_tilemap = function(tilemap){
+	// 	var start_x = (-this.tile_width*18);
+	// 	var start_y = (-this.tile_width*18);
+	// 	var cube_size = 36;
+
+	// 	for(var ix = start_x; ix < this.tile_width*18; ix+=this.tile_width){
+	// 		for(var iy = start_y; iy < this.tile_width*18; iy+=this.tile_width){
+
+	// 			// Toss out tiles outside the window
+	// 			if( ((ix + iy) / 2) < -((this.window_height/2)+(this.tile_width*2)) ||
+	// 				((ix + iy) / 2) > ((this.window_height/2)+(this.tile_width*2))  ||
+	// 				(ix - iy) < -((this.window_width/2)+(this.tile_width*2)) ||
+	// 				(ix - iy) > ((this.window_width/2)+(this.tile_width*2))
+	// 			){
+	// 				continue;
+	// 			}
+
+	// 			var tmp_x = ((ix+(this.tile_width*18)) / this.tile_width);
+	// 			var tmp_y = ((iy+(this.tile_width*18)) / this.tile_width);
+	// 			var tmp_i = (tmp_x*cube_size)+tmp_y;
+	// 			var height = heightmap[tmp_i].height;
+
+	// 			if(height > 0.8){
+	// 				this.terrain_ctx.fillStyle = luminance('#7A8781', height - 0.8);
+	// 			} else if(height > 0.7){
+	// 				this.terrain_ctx.fillStyle = luminance('#59842A', height - 0.7);
+	// 			} else if(height > 0.6){
+	// 				this.terrain_ctx.fillStyle = luminance('#4C7124', height - 0.6);
+	// 			} else if(height > 0.57){
+	// 				this.terrain_ctx.fillStyle = luminance('#326800', height - 0.57);
+	// 			} else {
+	// 				this.terrain_ctx.fillStyle = luminance('#254e78', height);
+	// 			}
+
+	// 			this.terrain_ctx.beginPath();
+	// 			this.draw_tile(ix+this.map_offset[0] ,iy+this.map_offset[1] );
+	// 			this.terrain_ctx.fill();
+	// 			// this.terrain_ctx.stroke();
+	// 		}
+	// 	}
+	// }
 
 	this.start_path = function(){
 		this.terrain_ctx.beginPath();
@@ -183,7 +277,7 @@ function UI(ui_canvas_id){
 			self.ui_ctx.fillStyle = 'rgba(0,100,0,0.5)';
 			var bounding_box = terrain.get_tile_corners(e.pageX, e.pageY);
 			var iso_coords = terrain.iso_to_cartesian(e.pageX - terrain.translation[0], e.pageY - terrain.translation[1]);
-			
+
 			iso_coords[0] = Math.floor(iso_coords[0] / terrain.tile_width);
 			iso_coords[1] = Math.floor(iso_coords[1] / terrain.tile_width);
 
@@ -198,7 +292,7 @@ function UI(ui_canvas_id){
 			self.ui_ctx.fill();
 
 			self.ui_ctx.fillStyle = "#000";
-			
+
 			var tmp_iso_display = iso_coords[0] + ', ' + iso_coords[1];
 			self.ui_ctx.fillText(tmp_iso_display, bounding_box[0][0] - 6, bounding_box[0][1] + 20);
 
